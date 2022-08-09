87a04a72c6af5d868d8797e990770d749687c2f3
Voote <kszygy@gmail.com>
2022-08-09 17:33:52 +0100

    month test converted from js to ts

--- src/components/month.test.ts
+++ src/components/month.test.ts
@@ -2,7 +2,7 @@ import { monthChecker } from './month';
 
 describe('month checker', () => {
   it('should return month name', () => {
-    const num = 11;
+    const num: string = '11';
 
     expect(monthChecker(num)).toBe('November');
   });
